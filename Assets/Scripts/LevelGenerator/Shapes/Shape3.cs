365659fc9ca9ca3f3076857d089bff3eb437b2fd
alon2000000 <alon2000000@gmail.com>
2017-10-02 19:32:14 +0300

    fixing the fence bug
    fixing bug:
    there are sometimes holes in the top wall of the fence

--- Assets/Scripts/LevelGenerator/Shapes/Shape3.cs
+++ Assets/Scripts/LevelGenerator/Shapes/Shape3.cs
@@ -19,7 +19,6 @@ namespace AssemblyCSharp
                 new Tuple<int,int>[]{ new Tuple<int,int>(0, 0), new Tuple<int,int>(0, 1), new Tuple<int,int>(0, 2), new Tuple<int,int>(1, 0) },
                 new Tuple<int,int>[]{ new Tuple<int,int>(0, 0), new Tuple<int,int>(1, 0), new Tuple<int,int>(2, 0), new Tuple<int,int>(0, 1) },
                 new Tuple<int,int>[]{ new Tuple<int,int>(0, 1), new Tuple<int,int>(1, 1), new Tuple<int,int>(2, 1), new Tuple<int,int>(1, 0) },
-                new Tuple<int,int>[]{ new Tuple<int,int>(1, 0), new Tuple<int,int>(1, 1), new Tuple<int,int>(1, 2), new Tuple<int,int>(0, 2) }
             };
         }
     }
