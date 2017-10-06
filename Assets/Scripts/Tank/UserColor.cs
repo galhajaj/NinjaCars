4d1d084d1233cf00914197529ff957b0ac24a180
galhajaj <galhajaj@gmail.com>
2017-10-06 08:52:55 +0300

    Red blue colors for players + delete some old sprites from project

---
+++ Assets/Scripts/Tank/UserColor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class UserColor : NetworkBehaviour
+{
+
+	void Start ()
+    {
+        if (!isLocalPlayer)
+            Destroy(this);
+
+        if (isLocalPlayer)
+            this.gameObject.GetComponent<SpriteRenderer>().color = Color.blue;
+        else
+            this.gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+    }
+	
+	void Update ()
+    {
+		
+	}
+}
