e7b34c845757d208a0a90b7dbc86e55b0f06151b
galhajaj <galhajaj@gmail.com>
2017-10-12 22:31:49 +0300

    add reload skill

---
+++ Assets/Scripts/Chips/ChipReload.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChipReload : Chip
+{
+    protected override void executeStart()
+    {
+        UserShooting userShootingScript = Players.Instance.GetLocal().GetComponent<UserShooting>();
+        userShootingScript.AmmoCount = userShootingScript.ClipMaxSize;
+    }
+}
